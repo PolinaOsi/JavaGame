99a759aec5f4e759fb8801b31ccf56945d3ee80c
Polina Osipova <76431671+PolinaOsi@users.noreply.github.com>
2021-06-09 12:44:30 +0700

    Update Main.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,6 +3,7 @@ package sample;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+
 public class Main extends Application {
 
     public static void main(String[] args) { launch(args); }
@@ -11,6 +12,11 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        if (model.GetSize() > 9 || model.GetSize() < 2)
+        {
+            System.out.println("Incorrect size!");
+            System.exit(-1);
+        }
         Stage window;
         window = primaryStage;
         window.setTitle("2048");
